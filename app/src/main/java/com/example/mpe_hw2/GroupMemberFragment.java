0e5a5e5df919045700f4f2c8a28df0dfe7098984
Aigars Strods <strods.aigars@gmail.com>
2021-11-16 21:47:37 +0200

    Toast message impelementation

--- app/src/main/java/com/example/mpe_hw2/GroupMemberFragment.java
+++ app/src/main/java/com/example/mpe_hw2/GroupMemberFragment.java
@@ -2,7 +2,12 @@ package com.example.mpe_hw2;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,11 +17,29 @@ public class GroupMemberFragment extends DialogFragment {
     @NonNull
     @Override
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
-        return new AlertDialog.Builder(requireContext())
-                .setMessage(getString(R.string.order_confirmation))
-                .setPositiveButton(getString(R.string.ok), (dialog, which) -> {} )
+        AlertDialog alertDialog = new AlertDialog.Builder(requireContext())
+                .setMessage(R.string.order_confirmation)
+                .setPositiveButton(R.string.ok, null)
+                .setNegativeButton(R.string.close, (dialog, which) -> showToast("You closed dialog"))
                 .create();
+
+        alertDialog.setOnShowListener(dialog -> {
+            ((AlertDialog) dialog)
+                    .getButton(AlertDialog.BUTTON_POSITIVE)
+                    .setOnClickListener(view -> showToast("You clicked OK"));
+        });
+
+        return alertDialog;
     }
 
+
     public static String TAG = "GroupMemberDialog";
+
+    private void showToast(String text) {
+        Context context = getContext();
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
 }
